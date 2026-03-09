e6de4b5649d9334cb619411d331ba94fe2b71915
Kiril Stavrev <kstavreb@gmail.com>
2026-03-09 12:24:01 +0200

    Update Biblioteka.py

--- Biblioteka.py
+++ Biblioteka.py
@@ -1,75 +1,66 @@
-import steamlit as st
-st.title("moqta mini biblioteka")
-if "boobks" not in st.session_state:
-  st.session_state_books = []
+import streamlit as st
 
+st.title("Moqta mini biblioteka")
 
-st.header("dobaci kniga")
-title = st.text_input("zaglavie")
-author = st.text_input("avtor")
-price = st.number_input("cena", min_value=0.0)
-if st.button("dobavi knigata"):
+if "books" not in st.session_state:
+    st.session_state.books = []
 
- book = {
-  "title": title,
-  "author": author,
-  "price": price
- }
+st.header("Dobavi kniga")
+title = st.text_input("Zaglavie")
+author = st.text_input("Avtor")
+price = st.number_input("Cena", min_value=0.0)
 
-st.session_state.books.append(book)
-st.success("knigata e dobavena!")
+if st.button("Dobavi knigata"):
+    if title and author:
+        book = {
+            "title": title,
+            "author": author,
+            "price": price
+        }
+        st.session_state.books.append(book)
+        st.success("Knigata e dobavena!")
+    else:
+        st.error("Populnete vsichki poleta!")
 
-if st.button("pokJI VSICHKI KNIGI"):
+if st.button("Pokaji vsichki knigi"):
+    if len(st.session_state.books) == 0:
+        st.write("Nqma dobaveni knigi")
+    else:
+        for book in st.session_state.books:
+            st.write(f"Zaglavie: {book['title']}")
+            st.write(f"Avtor: {book['author']}")
+            st.write(f"Cena: {book['price']}")
+            st.write("------------------")
 
- if len(st.session_state.books) == 0:
-  st.write("nqma dobaveni knigi")
-else:
- for book in st.session_state.books:
-  st.write("zaglavie", book["title"])
-st.write("avtor", book["author"])
-st.write("cena", book["price"])
-st.write("------------------")
+st.header("Tursene na avtor")
+search_author = st.text_input("Vuvedi ime na avtor")
+if st.button("Tursi po avtor"):
+    found = False
+    for book in st.session_state.books:
+        if book["author"].lower() == search_author.lower():
+            st.write(book)
+            found = True
+    if not found:
+        st.write("Nqma namereni knigi ot tozi avtor")
 
-st.header("tursene na avtor")
-search_author = st.text_input("vuvedi ime na avtor")
-if st.button("tursi po avtor"):
+st.header("Tursene po zaglavie")
+search_title = st.text_input("Vuvedi zaglavie")
+if st.button("Tursi po zaglavie"):
+    found = False
+    for book in st.session_state.books:
+        if book["title"].lower() == search_title.lower():
+            st.write(book)
+            found = True
+    if not found:
+        st.write("Nqma namerena takava kniga.")
 
- found = False
-
-for book in st.session_state.books:
-  if book["author"] == search_author:
-   st.write(book)
-  found = True
-
-if found == False:
- st.write("nqma namereni knigi ot tozi avtor")
-
-
-
-st.header("tursene po zaglavie")
-search_title = st.text_input("vuvedi")
-if st.button("tursi po zaglavie"):
- found = True
-
-for book in st.session_state.books:
- if book["title"] == search_title:
-   st.write(book)
-   found = True
-
-if found == False:
-  st.write("nqma namerena takava kniga.")
-
-if st.button("pokaji nai evtina kniga"):
- else:
-  cheapest = st.sseassion_state.books[0]
-
-for book in st.session_state.books:
-  if book["price"] < cheapest["price"]:
-    cheapest = book
-
-st write("nai evtina kn")
-st.write(cheapest)
-
-
-
-  
+if st.button("Pokaji nai-evtina kniga"):
+    if len(st.session_state.books) == 0:
+        st.write("Bibliotekata e prazna")
+    else:
+        cheapest = st.session_state.books[0]
+        for book in st.session_state.books:
+            if book["price"] < cheapest["price"]:
+                cheapest = book
+        st.write("Nai-evtina kniga:")
+        st.write(cheapest)
