0436318b8ed489a74799b26cdb8b82b5a301ff38
Kiril Stavrev <kstavreb@gmail.com>
2026-03-09 12:12:53 +0200

    Update Biblioteka.py

--- Biblioteka.py
+++ Biblioteka.py
@@ -38,7 +38,7 @@ if st.button("tursi po avtor"):
 
 for book in st.session_state.books:
   if book["author"] == search_author:
-  st.write(book)
+   st.write(book)
   found = True
 
 if found == False:
